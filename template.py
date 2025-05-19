6ad22bff40d04a5b2a4a2f00f8ecaec1a5a6e9ff
Thomas Hagedorn <55408937+thaggy@users.noreply.github.com>
2025-05-18 21:10:44 -0500

    Adding async support and .har file`

--- template.py
+++ template.py
@@ -1,16 +1,26 @@
+import g4f.client
 import speech_recognition as sr
 import keyboard
 import pyttsx3
 import g4f
 import traceback
+import asyncio
 
-# Stuff to help with request? Idk doesn't work for me very well on my crappy mac laptop where I did the initial writing for this
-# TODO: Test this on my windows machine. My mac doesn't work very well and I don't have docker setup for this yet
-if False:
-    import os
+# Get Async stuff to be happy : )
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-    os.environ['CURL_CA_BUNDLE'] = ''
-    os.environ['REQUESTS_CA_BUNDLE'] = ''
+async def generate_prompt():
+    chat_completion = await client.chat.completions.create(model="gpt-4o-mini", 
+                                                            # max_tokens=100, 
+                                                            temperature=0.9, 
+                                                            top_p=1, 
+                                                            top_k=0,
+                                                            messages=conversation_history, 
+                                                            stream=False)
+
+    # Grab the message content from chat gpt
+    response = chat_completion.choices[0].message.content
+    return response
 
 # Exit Conditions
 def exit_conditions():
@@ -45,48 +55,43 @@ system_prompt = "You are a helpful assistant chatbot trained by OpenAI.\
 conversation_history = [{"role": "system", "content": system_prompt}]
 
 # Setup g4f client
-client = g4f.Client()
+client = g4f.client.AsyncClient()
 
 print('Setup complete!')
-# Setup conditions to kill the program
-while not exit_conditions():
 
-    # If the conditions have been met to record audio...
-    if record_conditions():
+async def main():
+    # Setup conditions to kill the program
+    while not exit_conditions():
 
-        # Get audio from microphone
-        with sr.Microphone() as source:
+        # If the conditions have been met to record audio...
+        if record_conditions():
 
-            # Get audio from user...
-            print('Recording input...')
-            audio = recognizer.listen(source, phrase_time_limit=100)
+            # Get audio from microphone
+            with sr.Microphone() as source:
 
-        try:
-            # Parse Audio
-            recognized_text = recognizer.recognize_google(audio, language="en-US")
-            print('User Recognized Speech : ' + recognized_text)
+                # Get audio from user...
+                print('Recording input...')
+                audio = recognizer.listen(source, phrase_time_limit=100)
 
-            # Send conversation to Chat GPT
-            conversation_history.append({"role": "user", "content": f"{recognized_text}"})
-            chat_completion = client.chat.completions.create(model="gpt-4o-mini", 
-                                                            # max_tokens=100, 
-                                                            temperature=0.9, 
-                                                            top_p=1, 
-                                                            top_k=0,
-                                                            messages=conversation_history, 
-                                                            stream=False)
+            try:
+                # Parse Audio
+                recognized_text = recognizer.recognize_google(audio, language="en-US")
+                print('User Recognized Speech : ' + recognized_text)
 
-            # Grab the message content from chat gpt
-            response = chat_completion.choices[0].message.content
-            print("ChatGPT : " + response)
+                # Send conversation to Chat GPT
+                conversation_history.append({"role": "user", "content": f"{recognized_text}"})
+                response = await generate_prompt()
+                print("ChatGPT : " + response)
 
-            # Say message in TTS
-            tts.say(response)
-            tts.runAndWait()
+                # Say message in TTS
+                tts.say(response)
+                tts.runAndWait()
 
-            # Add it to the conversation history
-            conversation_history.append({'role' : 'bot', 'content' : f"{response}"})
+                # Add it to the conversation history
+                conversation_history.append({"role" : 'assistant', "content" : f"{response}"})
 
-        except Exception:
-            print('Error handling here')
-            traceback.print_exc()
+            except Exception:
+                print('Error handling here')
+                traceback.print_exc()
+
+asyncio.run(main())
